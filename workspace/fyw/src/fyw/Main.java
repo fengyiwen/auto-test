625dfe6ad63d89509abbc6ef01630c0be7823fa4
yiwen.feng <fengyiwen_79@sina.cn>
2016-08-25 09:31:55 +0800

    test

---
+++ workspace/fyw/src/fyw/Main.java
@@ -0,0 +1,18 @@
+package fyw;
+
+public class Main {
+	
+	public String test;
+	public void a(){
+		if (test.equals("")){
+			System.out.println("");
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+}
